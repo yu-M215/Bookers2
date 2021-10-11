6746e9fba66971f610dd78c145916e81007ba017
yu-M215 <yuu0609km.215@gmail.com>
2021-10-11 06:00:18 +0000

    [Update] booksのindexページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,24 @@
 class BooksController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+    @user = User.find(current_user.id)
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end

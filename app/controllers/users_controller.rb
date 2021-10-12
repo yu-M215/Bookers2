67db5c9d6ee1930da9f541dce3142a30782fe29f
yu-M215 <yuu0609km.215@gmail.com>
2021-10-12 08:28:21 +0000

    [Update] プロフィール更新時のエラーメッセージ表示設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,10 +16,13 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(user.id)
-    flash[:notice] = "You have updated user successfully."
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+      flash[:notice] = "You have updated user successfully."
+    else
+      render :edit
+    end
   end
 
   private

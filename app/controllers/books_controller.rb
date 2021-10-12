5e0de58d17b6bd8ab4af6a450a3c85cc4b9fc295
yu-M215 <yuu0609km.215@gmail.com>
2021-10-12 08:16:01 +0000

    [Update] Book編集時のエラーメッセージ表示設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,9 +32,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
-    flash[:notice] = "You have updated book successfully."
+    if @book.update(book_params)
+        redirect_to book_path(@book.id)
+        flash[:notice] = "You have updated book successfully."
+    else
+      render :edit
+    end
   end
 
   def destroy

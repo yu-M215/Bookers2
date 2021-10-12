eb012b1625bab4671700e366ea159e226f8d97f2
yu-M215 <yuu0609km.215@gmail.com>
2021-10-12 10:45:43 +0000

    [Finish] 4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,7 +22,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @new_book = Book.new
-    @user = User.find(current_user.id)
+    @user = User.find(@book.user.id)
 
   end
 

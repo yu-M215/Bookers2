77da27675add97a95f9c348052c89d8371662fe9
yu-M215 <yuu0609km.215@gmail.com>
2021-10-17 07:15:33 +0000

    [Update] BookCommentsのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
+    resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
 end

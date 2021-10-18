78db12da35869ba48c0aa026c0090e87e442c72a
yu-M215 <yuu0609km.215@gmail.com>
2021-10-18 05:33:48 +0000

    [Add] searchアクション、search_for定義

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,27 @@
 class SearchesController < ApplicationController
 
   def search
+    @model = params["model"]
+    @method = params["method"]
+    @content = params["content"]
+    @records = search_for(@model,@method,@content)
+  end
+
+  private
+
+  def search_for(model,method,content)
+    if model == 'user'
+      if method == 'perfect'
+        User.where(name: content)
+      else
+        User.where('name LIKE?': '%'+content+'%')
+      end
+    elsif model == 'book'
+      if method == 'perfect'
+        Book.where(title: content)
+      else
+        Book.where('title LIKE?': '%'+content+'%')
+      end
   end
 
 end

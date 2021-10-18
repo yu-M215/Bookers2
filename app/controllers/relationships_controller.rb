dc1d79a9f1b5836ee3bff25ba0e8ed5e1b4b88a5
yu-M215 <yuu0609km.215@gmail.com>
2021-10-18 03:25:05 +0000

    [Update] relationshipsコントローラにアクション定義

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,23 @@
 class RelationshipsController < ApplicationController
   def create
+    current_user.follow(params[:user_id])
+    redirect_back fallback_location: {action: "index"}
   end
 
   def destroy
+    current_user.unfollow(params[:user_id])
+    redirect_back fallback_location: {action: "index"}
+  end
+
+  #followしているユーザーの一覧取得
+  def followeds
+    user = User.find(params[:user_id])
+    @users = user.followeds
+  end
+
+  #followされているユーザーの一覧取得
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
   end
 end

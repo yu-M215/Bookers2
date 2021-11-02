49a2f7c84a38286e34acccb91cb3f30faa5bc33e
yu-M215 <yuu0609km.215@gmail.com>
2021-11-02 01:48:43 +0000

    [Add] DM機能関連のモデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_17_102356) do
+ActiveRecord::Schema.define(version: 2021_11_02_014744) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -35,9 +35,29 @@ ActiveRecord::Schema.define(version: 2021_10_17_102356) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "room_id"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "relationships", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_rooms", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "room_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
